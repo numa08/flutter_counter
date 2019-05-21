364668242d75abaae7f9f5877f1d8f6d6259ccb2
numa08 <n511287@gmail.com>
2019-05-21 21:59:59 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'CounterBloc.dart';
+import 'CounterPage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+
+  final CounterBloc _counterBloc = CounterBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: BlocProvider<CounterBloc>(
+        bloc: _counterBloc,
+        child: CounterPage(),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _counterBloc.dispose();
+    super.dispose();
+  }
+}
